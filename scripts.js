53388f72f338317454d03bff91433ce7434c0e45
Heyzofi <harsanyizsofi@hotmail.com>
2021-07-12 11:57:49 +0800

    hamburger

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    
+
+    $('.hamburger-button').click(function(){
+        $('.mobile-menu').slideToggle();
+        $(this).toggleClass('active');
+      });
+
+      
+});
