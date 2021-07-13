4d43669e99bbcea44f295ea52fb40730bb081335
Heyzofi <harsanyizsofi@hotmail.com>
2021-07-13 16:39:36 +0800

    jekyllify

--- scripts.js
+++ scripts.js
@@ -7,5 +7,9 @@ $( document ).ready(function() {
         $(this).toggleClass('active');
       });
 
-      
+      // Animate on scroll activate
+      AOS.init({
+        duration: 500 // values from 0 to 3000, with step 50ms
+      });
+
 });
